c822edf5776105adaf022530b8ff9fc93592dd20
Muhammad Abdullah <binarywren@gmail.com>
2024-07-21 23:49:48 -0700

    Add files via upload

---
+++ coloringtext.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is ABdullah "+ Fore.YELLOW+ Back.BLUE+"I am a python developer")
+print(Back.CYAN+"Hi My name is Muhammad Abdullah")
+print(Fore.RED + Back.GREEN+ "Hi My name is Muhammad Abdullah")
